1cc9fbf698ea976d393f1d42121d26606e3fd2b1
Your Namebower <bower.jacob@gmail.com>
2014-10-26 19:58:52 -0700

    Header
    Includes header

---
+++ includes/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>200 Orphanages Worldwide</title>
+
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+	
+
+		<header>
+			<div class="header-wrapper">
+				<div class="header-left">
+					<a href="#" ><img src="" alt="200 Orphanages Worldwide logo" class="logo" /></a>
+				</div>
+				<div class="header-right">
+					<div class="responsive-menu-wrapper">
+						<a href="#" class="responsive-menu"><img src="img/menu-img.png" alt="responsive menu image" class="responsive-menu-img"/></a>
+					</div>
+					<div class="share-buttons">
+						<ul class="share-button-list">
+							<li><a href="#" >Donate</a></li>
+							<li><a href="#" >Volunteer</a></li>
+							<li><a href="#" >Share</a></li>
+						</ul>
+					</div>
+					<div class="header-nav-wrapper">
+						<nav class="header-nav">
+							<ul class="header-nav-list">
+								<li><a href="#" >Projects</a></li>
+								<li><a href="#" >Volunteer</a></li>
+								<li><a href="#" >Events</a></li>
+								<li><a href="#" >About</a></li>
+								<li><a href="#" >Gallery</a></li>
+								<li><a href="#" >Blog</a></li>
+								<li><a href="#" >Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div><!-- .header-wrapper -->
+			<div class="clearfix"></div>
+		</header>
+		
+	
+	<div id="wrapper">
