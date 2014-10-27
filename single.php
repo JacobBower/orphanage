9e034ace0399bce3c62221ad0d46d840b8cf558c
Your Namebower <bower.jacob@gmail.com>
2014-10-26 19:59:09 -0700

    Single.php
    Single page layout

---
+++ single.php
@@ -0,0 +1,73 @@
+<?php include "includes/header.php"; ?>
+
+	<div id="single-content">
+    
+		<section id="single-post">
+        	<h2>This is the Tile</h2>
+            <h5><em>Posted October 23, 2014 by Weird Al under <a href="#">Tag</a> <a href="#">Tag</a> <a href="#">Tag</a>
+            </em></h5>
+            	<div  id="single-share">
+					<ul>
+						<li><a href="#">Share</a></li>
+						<li><a href="#0.1_">Share</a></li>
+						<li><a href="#0.1_">Share</a></li>
+					</ul>
+				</div>
+                	<p><em>
+                    	Pasta ipsum dolor sit amet shitty bavettine spirali agnolotti calamaretti. Perciatelli rigatoncini capellini farfalle 
+                        paccheri cellentani tortelloni fettuce fuckers asshat fusilli vermicelli sagne 'ncannulate fucker trenette campanelle. 
+                        Dick frak scialatelli cellentani lanterne. Lasagnotte mezzani pasta pasta al ceppo cavatappi fagioloni rat fart. 
+                    </p>
+                    <p>
+                    	Lasagnotte mezzani pasta pasta al ceppo cavatappi fagioloni rat fart. Frak creste di galli shitty sumbitch damn mezzani 
+                        pasta ravioli linguine tortiglioni ditalini bavettine trofie pennoni rotini. Zitoni lagane gnocchi trofie calamarata mostaccioli. 
+                        Creste di galli fagioloni sorprese fuck lasagne perciatelli.
+                    </p>
+                    <p>
+                    	Fettucelle booyah bitches farfalline bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi trofie campanelle gnocchi 
+                        vermicelli capunti orzo lagane gomito sacchettoni torchio. Quadrefiore frak chifferi linguettine lumaconi.
+                    </p></em>
+                    <br />
+                    <hr />
+                    <br />
+		</section>
+        
+     </div>   
+    
+    <div id="single-widget">
+        <div class="widget-category"><h2>Category</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Category</a></li>
+				<li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+			</ul>
+            <br />
+        <div class="widget-category"><h2>Links</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+    </div>
+    
+    <div id="single-post-comment">
+        	<br />
+      		<h3>Comments</h3>
+            	<div id="comment-image"><br /><br /><br /><a href="#">IMG</a></div>
+                	<br />
+                	<h4>Firstname Lastname</h4>
+                    <h5><em>October 23, 2014</em></h5>
+                    <br />
+                    	<p><em>Bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi 
+                        trofie campanelle gnocchi vermicelli capunti orzo lagane gomito sacchettoni.</em></p>	
+    </div>
+
+<?php include "includes/footer.php"; ?>
