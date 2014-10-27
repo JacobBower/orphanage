d683a169c3c28618a9e0c9ca6d5ce5c96590f4b6
Your Namebower <bower.jacob@gmail.com>
2014-10-26 19:57:22 -0700

    Footer
    Includes footer

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+</div> <!--WRAPPER-->
+	<div class="clearfix"></div>
+	<footer>
+		<div class="footer-wrapper">
+			<div class="footer-left">
+				<ul class="social-icons">
+					<li class="margin-right"><a href="#"><img src="img/linkedin-128.png" alt="linkedin icon" /></a></li>
+					<li class="margin-right"><a href="#"><img src="img/pinterest-128.png" alt="pinterest icon" /></a></li>
+					<li class="margin-right"><a href="#"><img src="img/facebook-128.png" alt="facebook icon" /></a></li>
+					<li><a href="#"><img src="img/twitter-128.png" alt="twitter icon" /></a></li>
+				</ul>
+
+				<ul class="share-button-list">
+					<li class="button-small margin-right"><a href="#" >Donate</a></li>
+					<li class="button-small margin-right"><a href="#" >Volunteer</a></li>
+					<li class="button-small"><a href="#" >Share</a></li>
+				</ul>
+			</div>
+			<aside class="footer-right">
+				<nav class="footer-nav">
+					<ul>
+						<li><a href="#" >Projects</a></li>
+						<li><a href="#" >Volunteer</a></li>
+						<li><a href="#" >Events</a></li>
+						<li><a href="#" >About</a></li>
+						<li><a href="#" >Gallery</a></li>
+						<li><a href="#" >Blog</a></li>
+						<li><a href="#" >Contact</a></li>
+					</ul>
+				</nav>
+				<p class="copy">All content &copy; 2014 200 Orphanages Worldwide</p>
+			</aside>
+		</div>
+		<div class="clearfix"></div>
+	</footer>
+</body>
+</html>
