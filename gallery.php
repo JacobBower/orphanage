f6f3e95d606cb5a7a2cebe117ad60f370c7b3dc3
Your Namebower <bower.jacob@gmail.com>
2014-10-27 17:58:00 -0700

    gallery.php 10/27/14 6pm

---
+++ gallery.php
@@ -0,0 +1,70 @@
+<?php include "includes/header.php"; ?>
+	
+    
+    
+	<div id="gallery-content">
+    
+ 	   <h2>Gallery</h2>
+       
+       <section id="gallery">
+            <h3>Content Title</h3>
+                	<p>
+                    	Pasta ipsum dolor sit amet shitty bavettine spirali agnolotti calamaretti. Perciatelli rigatoncini capellini farfalle 
+                        paccheri cellentani tortelloni fettuce fuckers asshat fusilli vermicelli sagne.Dick frak pasta al ceppo cavatappi fagioloni rat fart. 
+                    </p>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+       </section><!-----GALLERY----->
+       
+       <section id="gallery">
+            <h3>Content Title</h3>
+                	<p>
+                    	Pasta ipsum dolor sit amet shitty bavettine spirali agnolotti calamaretti. Perciatelli rigatoncini capellini farfalle 
+                        paccheri cellentani tortelloni fettuce fuckers asshat fusilli vermicelli sagne.Dick frak pasta al ceppo cavatappi fagioloni rat fart. 
+                    </p>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+                    <div class="placeholder"><a href="#"></a></div>
+       </section><!-----GALLERY----->
+       
+    </div><!-----GALLERY-CONTENT----->
+    
+    <div id="gallery-widget">
+        <div class="widget-category"><h2>Category</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Category</a></li>
+				<li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+			</ul>
+            <br />
+        <div class="widget-category"><h2>Links</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+    </div><!-----ABOUT-WIDGET----->
+
+
+    
+
+<?php include "includes/footer.php"; ?>
