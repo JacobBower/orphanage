1112b782e798fae99566b3e7b9a9f76bae3afb5f
Your Namebower <bower.jacob@gmail.com>
2014-10-27 17:26:54 -0700

    About Page 10/27/14
    Added about.php page not yet responsive

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php include "includes/header.php"; ?>
+	
+    
+    
+	<div id="about-content">
+    
+ 	   <h2>200 Orphanages Worldwide</h2>
+       
+       <section id="about-mission">
+            <h3>Our Mission Statement</h3>
+                	<p>
+                    	Pasta ipsum dolor sit amet shitty bavettine spirali agnolotti calamaretti. Perciatelli rigatoncini capellini farfalle 
+                        paccheri cellentani tortelloni fettuce fuckers asshat fusilli vermicelli sagne 'ncannulate fucker trenette campanelle. 
+                        Dick frak scialatelli cellentani lanterne. Lasagnotte mezzani pasta pasta al ceppo cavatappi fagioloni rat fart. 
+                    	Fettucelle booyah bitches farfalline bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi trofie campanelle gnocchi 
+                        vermicelli capunti orzo lagane gomito sacchettoni torchio. Quadrefiore frak chifferi linguettine lumaconi.
+                    </p>
+        </section>
+		<section id="about-leadership">
+      		<h3>Our Leadership</h3>
+            	<section id="about-leadership-post">
+            		<div class="placeholder"><a href="#"></a></div>
+                		<br />
+                        <h4>Title</h4>
+                    	<p>Bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi 
+                        trofie campanelle gnocchi vermicelli capunti orzo.<br />
+                        <a href="" class="button">Button</a>
+                        </p>
+                </section><!-----ABOUT-LEADERSHIP-POST----->
+                <section id="about-leadership-post">
+            		<div class="placeholder"><a href="#"></a></div>
+                		<br />
+                        <h4>Title</h4>
+                    	<p>Bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi 
+                        trofie campanelle gnocchi vermicelli capunti orzo.<br />
+                        <a href="" class="button">Button</a>
+                        <br />
+                        <a href="" class="button">More</a>
+                        </p>
+                </section><!-----ABOUT-LEADERSHIP-POST----->
+                	
+    	</section><!-----ABOUT-LEADERSHIP----->
+		
+        <section id="about-partners">
+        	<h3>Our Partners</h3>
+            	<div id="partners">
+                	<p>
+                    <h4>Title</h4>
+                    	<div id="partner-logo">Logo</div>
+                        <p>Gnocchi trofie campanelle gnocchi 
+                        vermicelli capunti orzo lagane.<br />
+                        <a href="" class="button">Button</a>
+                        </p>
+                    </p>
+                </div><!-----PARTNERS----->
+                <br />
+            	<div id="partners">
+                	<h4>Title</h4>
+                    	<div id="partner-logo">Logo</div>
+                        <p>Gnocchi trofie campanelle gnocchi 
+                        vermicelli capunti orzo lagane.<br />
+                        <a href="" class="button">Button</a>
+                        </p>
+                </div><!-----PARTNERS----->
+        
+        </section><!-----ABOUT-PARTNERS----->
+
+     </div><!-------ABOUT-CONTENT------  -->
+    
+    <div id="about-widget">
+        <div class="widget-category"><h2>Category</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Category</a></li>
+				<li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Category</a></li>
+			</ul>
+            <br />
+        <div class="widget-category"><h2>Links</h2></div>
+        	<br />
+        	<ul>
+				<li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+    </div><!-----ABOUT-WIDGET----->
+
+
+    
+
+<?php include "includes/footer.php"; ?>
