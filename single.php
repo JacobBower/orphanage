2f247baf79209dad085c46531a3c9155d1065888
Your Namebower <bower.jacob@gmail.com>
2014-11-01 20:07:36 -0700

    changed single.php and css responsive
    changed id’s into class’s, made responsive

--- single.php
+++ single.php
@@ -1,19 +1,19 @@
 <?php include "includes/header.php"; ?>
 
-	<div id="single-content">
+	<div class="single-content">
     
 		<section id="single-post">
         	<h2>This is the Tile</h2>
             <h5><em>Posted October 23, 2014 by Weird Al under <a href="#">Tag</a> <a href="#">Tag</a> <a href="#">Tag</a>
             </em></h5>
-            	<div  id="single-share">
+            	<div  class="single-share">
 					<ul>
 						<li><a href="#">Share</a></li>
 						<li><a href="#0.1_">Share</a></li>
 						<li><a href="#0.1_">Share</a></li>
 					</ul>
 				</div>
-                	<p><em>
+                	<p>
                     	Pasta ipsum dolor sit amet shitty bavettine spirali agnolotti calamaretti. Perciatelli rigatoncini capellini farfalle 
                         paccheri cellentani tortelloni fettuce fuckers asshat fusilli vermicelli sagne 'ncannulate fucker trenette campanelle. 
                         Dick frak scialatelli cellentani lanterne. Lasagnotte mezzani pasta pasta al ceppo cavatappi fagioloni rat fart. 
@@ -26,7 +26,7 @@
                     <p>
                     	Fettucelle booyah bitches farfalline bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi trofie campanelle gnocchi 
                         vermicelli capunti orzo lagane gomito sacchettoni torchio. Quadrefiore frak chifferi linguettine lumaconi.
-                    </p></em>
+                    </p>
                     <br />
                     <hr />
                     <br />
@@ -34,7 +34,7 @@
         
      </div>   
     
-    <div id="single-widget">
+    <div class="widget">
         <div class="widget-category"><h2>Category</h2></div>
         	<br />
         	<ul>
@@ -57,17 +57,19 @@
 				<li><a href="#">Link</a></li>
 			</ul>
     </div>
+  
+  <div class="clearfix"></div>
     
-    <div id="single-post-comment">
+    <div class="post-comment">
         	<br />
       		<h3>Comments</h3>
-            	<div id="comment-image"><br /><br /><br /><a href="#">IMG</a></div>
+            	<div class="comment-image"><br /><br /><br /><a href="#">IMG</a></div>
                 	<br />
                 	<h4>Firstname Lastname</h4>
                     <h5><em>October 23, 2014</em></h5>
                     <br />
-                    	<p><em>Bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi 
-                        trofie campanelle gnocchi vermicelli capunti orzo lagane gomito sacchettoni.</em></p>	
+                    	<p>Bavettine penne rigate bitches fiori lagane trennette mafalde. Gnocchi 
+                        trofie campanelle gnocchi vermicelli capunti orzo lagane gomito sacchettoni.</p>	
     </div>
 
 <?php include "includes/footer.php"; ?>
