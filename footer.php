5c000272ef87129b7ef35a44674c2719b2ac563d
Your Namebower <bower.jacob@gmail.com>
2014-11-16 21:45:12 -0800

    added new social media icon links

--- footer.php
+++ footer.php
@@ -22,10 +22,10 @@
             
             <div class="footer-left column-4 no-pad-left no-pad-right">
 				<ul class="social-icons">
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin.png" alt="linkedin icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/pintrast.png" alt="pinterest icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/facebook.png" alt="facebook icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/twitter.png" alt="twitter icon" /></a></li>
+					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin@2.png" alt="linkedin icon" /></a></li>
+					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/pintrest@2.png" alt="pinterest icon" /></a></li>
+					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/facebook@2.png" alt="facebook icon" /></a></li>
+					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/twitter@2.png" alt="twitter icon" /></a></li>
 				</ul>
 			</div>
             
