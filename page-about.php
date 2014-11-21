2d8eb64048251e54c20736bd19c6bf4f903abde5
Anthony Keller <antkeller1@gmail.com>
2014-11-21 12:22:24 -0800

    edit main content section of about page

--- page-about.php
+++ page-about.php
@@ -14,10 +14,7 @@ get_header(); ?>
         </div>
        
         <section class="content-section border-bottom column-8 no-pad-left no-pad-right">
-                <h3>Our Mission Statement</h3>
-                	<p>
-                    	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+                <?php the_content(); ?>
         </section>
 		<section class="content-section border-bottom column-8 no-pad-left">
       		<h3>Our Leadership</h3>
