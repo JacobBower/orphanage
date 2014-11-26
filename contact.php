4e27fd800734025c5d3d55adf1c58f0d677e2b1d
Anthony Keller <antkeller1@gmail.com>
2014-11-26 10:38:59 -0800

    remove sidebar from contact page

--- contact.php
+++ contact.php
@@ -8,12 +8,8 @@ get_header(); ?>
     
         <h1><?php the_title(); ?></h1>
         <div class="row">
-
-        <div class="sidebar column-4 no-pad-right">
-            <?php get_sidebar('secondary'); ?>
-        </div>
        
-        <section class="content-section column-8 no-pad-left no-pad-right">
+        <section class="content-section column-12 no-pad-left no-pad-right">
 
         <?php the_content(); ?>
                 
