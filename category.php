c8233cd43b51b5eb282c734a7f3334bcdcec58a4
Your Namebower <bower.jacob@gmail.com>
2014-11-28 21:14:33 -0800

    edited unnecessary code

--- category.php
+++ category.php
@@ -35,13 +35,3 @@ get_header(); ?>
 	</div> <!-- CONTENT -->
 
 <?php get_footer(); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
