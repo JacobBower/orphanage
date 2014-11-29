58935052eb7477119d0589f9d903a49cf71ddf15
Your Namebower <bower.jacob@gmail.com>
2014-11-28 20:16:50 -0800

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+        <div class="content row">
+    		
+            <h1>Oooops!</h1>
+            
+               	<div>
+                    <p>Sorry, the page you are looking for doesn't exist. We all make mistakes.
+                     Please try the search form below. Thank you.</p>
+                    <?php get_search_form(); ?>
+                </div>
+                <br>
+        </div>
+
+</div>  <!--CLOSE WRAPPER-->
+<?php wp_footer(); ?>
+</body>
+</html>
