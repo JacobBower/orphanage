0eafc3eed77eb230db6336d502a5832df23e8c5f
Anthony Keller <antkeller1@gmail.com>
2014-12-02 19:47:16 -0800

    change name of contact page

--- page-contact.php
+++ page-contact.php
@@ -7,15 +7,13 @@ get_header(); ?>
 	<div class="content">
     
         <h1><?php the_title(); ?></h1>
-        <div class="row">
        
-        <section class="content-section column-12 no-pad-left no-pad-right">
+            <section class="content-section column-8 no-pad-left no-pad-right">
 
-        <?php the_content(); ?>
-                
-        </section>
+                <?php the_content(); ?>
+                    
+            </section>
 		
-        </div>
      </div><!-- CONTACT CONTENT -->
 
 <?php get_footer(); ?>
