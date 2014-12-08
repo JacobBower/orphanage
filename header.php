5f6c6147811c1c2f289697b4cbb19b98c58d72c9
Your Namebower <bower.jacob@gmail.com>
2014-12-08 14:18:57 -0800

    Added tracking code

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
 		<meta name="description" content="<?php echo strip_tags(get_the_excerpt()); ?>" />
 		<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0" />
 		<script src="<?php bloginfo('template_directory'); ?>/js/jquery.min.2.1.1.js"></script>
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		 
+		  ga('create', 'UA-1530997-15', 'auto');
+		  ga('send', 'pageview');
+		</script>
 		<?php $host = $_SERVER['SERVER_NAME']; if ($host == 'localhost') : ?>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
 		<?php else: ?>
