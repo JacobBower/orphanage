298102259c6029876aa9ad8516f71d2085241152
Anthony Keller <antkeller1@gmail.com>
2014-12-08 15:49:09 -0800

    add google analytics code to footer

--- footer.php
+++ footer.php
@@ -50,6 +50,18 @@
 	</footer>
 <?php wp_footer(); ?>
 
+<!-- google analytics -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-1530997-15', 'auto');
+	ga('send', 'pageview');
+ 
+</script>
+<!-- mobile menu -->
 <script type="text/javascript" charset="utf-8">
 	jQuery(window).load(function() { // enable function upon window load
 
@@ -59,7 +71,7 @@
 
 	});
 </script>
-
+<!-- home page image slider -->
 <script src="<?php bloginfo('template_directory'); ?>/js/owl.carousel.min.js"></script>
 <script>
 	jQuery(document).ready(function(){
