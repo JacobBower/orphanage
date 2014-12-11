71233ad06ecc8d677ed08d9d0dde575ade9e61ed
Eric Willis <ewillis182@gmail.com>
2014-12-10 22:42:44 -0800

    Put it Footer links and added _blank

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
 
 </div> <!--WRAPPER-->
-	
+
 	<footer>
     <section class="social-mobile">
 				<div class="row">
 					<ul class="social-icons">
-						<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin@2.png" alt="linkedin icon" /></a></li>
-						<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/pintrest@2.png" alt="pinterest icon" /></a></li>
-						<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/facebook@2.png" alt="facebook icon" /></a></li>
-						<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/twitter@2.png" alt="twitter icon" /></a></li>
+						<li class="column-3"><a href="https://www.linkedin.com/company/200-orphanages-worldwide" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin@2.png" alt="linkedin icon" /></a></li>
+						<li class="column-3"><a href="https://www.pinterest.com/200orphanages/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/pintrest@2.png" alt="pinterest icon" /></a></li>
+						<li class="column-3"><a href="https://www.facebook.com/200Orphans" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/facebook@2.png" alt="facebook icon" /></a></li>
+						<li class="column-3"><a href="https://twitter.com/janhan/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/twitter@2.png" alt="twitter icon" /></a></li>
 					</ul>
 				</div>
                 </section>
-    
+
 		<div class="footer-wrapper row">
-			
+
             <section class="social-mobile">
                 <ul class="share-button-list row">
 						<li class="button-footer column-4"><a href="#" class="yellow">Act</a></li>
@@ -22,25 +22,25 @@
 						<li class="button-footer column-4"><a href="#" class="red">Give</a></li>
 					</ul>
             </section>
-			
-            
+
+
             <div class="footer-left column-4 no-pad-left no-pad-right">
 				<ul class="social-icons">
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin@2.png" alt="linkedin icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/pintrest@2.png" alt="pinterest icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/facebook@2.png" alt="facebook icon" /></a></li>
-					<li class="column-3"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/twitter@2.png" alt="twitter icon" /></a></li>
+					<li class="column-3"><a href="https://www.linkedin.com/company/200-orphanages-worldwide" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/linkedin@2.png" alt="linkedin icon" /></a></li>
+					<li class="column-3"><a href="https://www.pinterest.com/200orphanages/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/pintrest@2.png" alt="pinterest icon" /></a></li>
+					<li class="column-3"><a href="https://www.facebook.com/200Orphans" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/facebook@2.png" alt="facebook icon" /></a></li>
+					<li class="column-3"><a href="https://twitter.com/janhan/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/twitter@2.png" alt="twitter icon" /></a></li>
 				</ul>
 			</div>
-            
-            
-                    
+
+
+
 			<aside class="footer-right column-8">
 				<nav class="footer-nav">
-					<?php wp_nav_menu( array( 
+					<?php wp_nav_menu( array(
 						'theme_location' => 'footer-menu' ,
 						'menu' => 'Footer Menu' ,
-						'container'  => 'ul', 
+						'container'  => 'ul',
 					) ); ?>
 				</nav>
 				<p class="copy">200 Orphanages Worldwide &copy; 2007 - <?php print (date ('Y')); ?></p>
@@ -69,7 +69,7 @@
 	    autoplay : true,
 	    autoplayTimeout : 3000,
 	    autoplayHoverPause : true,
-	    items : 1, 
+	    items : 1,
 		itemsDesktop : false,
 		itemsDesktopSmall : false,
 		itemsTablet : false,
